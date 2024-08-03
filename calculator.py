67240c8579528ce30d1b7411c1ca803ac03b834a
EugenTop4ik <159647861+EugenTop4ik@users.noreply.github.com>
2024-08-03 17:37:37 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+result=0
+print('Введите первое число')
+firstnum=int(input())
+print('Введите второе число')
+secondnum=int(input())
+print('Введите знак арифметической операции (+, -, *, /,)')
+sign=str(input())
+if sign == "+":
+    result=firstnum+secondnum
+elif sign == "-":
+    result=firstnum-secondnum
+elif sign == "*":
+    result=firstnum*secondnum
+elif sign == "/":
+    result=firstnum/secondnum
+else:
+    print ('Ошибка выполнения. Пожалуйста проверьте введённые данные')
+print (result)
+print ('Для завершения работы программы нажмине клавишу Enter')
+pause=str(input())
